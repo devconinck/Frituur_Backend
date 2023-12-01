4dda1e8e5c1c1a98b33c24b3d425dd5963ebcead
Quinten <quinten.deconinck@student.hogent.be>
2023-12-01 19:13:38 +0100

    Add tests for finding a product by id in controller and service

--- src/products/products.controller.spec.ts
+++ src/products/products.controller.spec.ts
@@ -17,4 +17,24 @@ describe('ProductsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should return an array of products', () => {
+    expect(controller.findAll()).toBeInstanceOf(Array);
+  });
+
+  it('should return a product with id 1', () => {
+    expect(controller.findOne('1')).toHaveProperty('id', '1');
+  });
+
+  it('should return a product with name "Product 1"', () => {
+    expect(controller.findOne('1')).toHaveProperty('name', 'Product 1');
+  });
+
+  it('should return a product with price 100', () => {
+    expect(controller.findOne('1')).toHaveProperty('price', 100);
+  });
+
+  it('should return a product with quantity 10', () => {
+    expect(controller.findOne('1')).toHaveProperty('quantity', 10);
+  });
 });
--- src/products/products.service.spec.ts
+++ src/products/products.service.spec.ts
@@ -15,4 +15,12 @@ describe('ProductsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return an array of products', () => {
+    expect(service.findAll()).toBeInstanceOf(Array);
+  });
+
+  it('should return a product with id 1', () => {
+    expect(service.findOne(1)).toHaveProperty('id', '1');
+  });
 });
