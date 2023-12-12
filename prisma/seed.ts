015bb531d9bea403110fedad32300acbf5038164
Quinten <quinten.deconinck@student.hogent.be>
2023-12-12 18:13:19 +0100

    finalised seed file and prisma schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,293 @@
 import { PrismaClient } from '@prisma/client';
+import { AuthService } from '../src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 const prisma = new PrismaClient();
+const jwtService = new JwtService();
+const prismaService = new PrismaService();
+const categorieNames = [
+  'Drinks',
+  'Fries',
+  'Preperations',
+  'Cold Sauces',
+  'Snacks',
+  'Bicky Burgers',
+  'Chicken/Turkey Snacks',
+];
+
+const productsData = [
+  {
+    name: 'Cola',
+    price: 2,
+    category: 'Drinks',
+    url: 'coke.png',
+  },
+  {
+    name: 'Cola Zero',
+    price: 2,
+    category: 'Drinks',
+    url: 'coke_zero.png',
+  },
+  {
+    name: 'Fanta',
+    price: 2,
+    category: 'Drinks',
+    url: 'fanta.png',
+  },
+  {
+    name: 'Sprite',
+    price: 2,
+    category: 'Drinks',
+    url: 'sprite.png',
+  },
+  {
+    name: 'Water',
+    price: 2,
+    category: 'Drinks',
+    url: 'water.png',
+  },
+  {
+    name: 'Water Bruis',
+    price: 2,
+    category: 'Drinks',
+    url: 'water_spark.png',
+  },
+  {
+    name: 'Ice Tea',
+    price: 2.2,
+    category: 'Drinks',
+    url: 'ice_tea.png',
+  },
+  {
+    name: 'Ice Tea Peach',
+    price: 2.2,
+    category: 'Drinks',
+    url: 'ice_tea_peach.png',
+  },
+  {
+    name: 'Ice Tea Zero',
+    price: 2.2,
+    category: 'Drinks',
+    url: 'ice_tea_zero.png',
+  },
+  {
+    name: 'Mini Friet',
+    price: 2.8,
+    category: 'Fries',
+    url: 'fries_mini.png',
+  },
+  {
+    name: 'Kleine Friet',
+    price: 3.3,
+    category: 'Fries',
+    url: 'fries_small.png',
+  },
+  {
+    name: 'Grote Friet',
+    price: 4,
+    category: 'Fries',
+    url: 'fries_big.png',
+  },
+  {
+    name: 'Super Friet',
+    price: 5.1,
+    category: 'Fries',
+    url: 'fries_super.png',
+  },
+  {
+    name: 'Stoofvlees',
+    price: 5.2,
+    category: 'Preperations',
+    url: 'stoofvlees.png',
+  },
+  {
+    name: 'Stoofvleessaus',
+    price: 2.5,
+    category: 'Preperations',
+    url: 'stoofvleessaus.png',
+  },
+  {
+    name: 'Balletjes in tomatensaus',
+    price: 5.1,
+    category: 'Preperations',
+    url: 'balletjes_tomaat.png',
+  },
+  {
+    name: 'Spaghettisaus',
+    price: 3.6,
+    category: 'Preperations',
+    url: 'spaghettisaus.png',
+  },
+  {
+    name: 'Americain',
+    price: 2,
+    category: 'Cold Sauces',
+    url: 'americain.png',
+  },
+  {
+    name: 'Bicky Saus',
+    price: 4.2,
+    category: 'Cold Sauces',
+    url: 'bicky_sauzen.png',
+  },
+  {
+    name: 'Cocktail',
+    price: 1,
+    category: 'Cold Sauces',
+    url: 'cocktail.png',
+  },
+  {
+    name: 'Curry Ketchup',
+    price: 1,
+    category: 'Cold Sauces',
+    url: 'ketchup_curry.png',
+  },
+  {
+    name: 'Mayonaise',
+    price: 1,
+    category: 'Cold Sauces',
+    url: 'mayo.png',
+  },
+  {
+    name: 'Joppie',
+    price: 1,
+    category: 'Cold Sauces',
+    url: 'joppie.png',
+  },
+  {
+    name: 'Special Saus',
+    price: 2,
+    category: 'Cold Sauces',
+    url: 'special_saus.png',
+  },
+  {
+    name: 'Bitterballen',
+    price: 2.8,
+    category: 'Snacks',
+    url: 'bitterbal.png',
+  },
+  {
+    name: 'Boulet',
+    price: 2.8,
+    category: 'Snacks',
+    url: 'boulet.png',
+  },
+  {
+    name: 'Cervela',
+    price: 3.3,
+    category: 'Snacks',
+    url: 'cervela.png',
+  },
+  {
+    name: 'Curryworst',
+    price: 2.3,
+    category: 'Snacks',
+    url: 'frikandel.png',
+  },
+  {
+    name: 'Mexicano',
+    price: 3.5,
+    category: 'Snacks',
+    url: 'mexicano.png',
+  },
+  {
+    name: 'Ribster',
+    price: 3.7,
+    category: 'Snacks',
+    url: 'ribster.png',
+  },
+  {
+    name: 'Sate',
+    price: 4.5,
+    category: 'Snacks',
+    url: 'sate.png',
+  },
+  {
+    name: 'Vleeskroket',
+    price: 2.8,
+    category: 'Snacks',
+    url: 'vleeskroket.png',
+  },
+  {
+    name: 'Bicky Burger',
+    price: 4.2,
+    category: 'Bicky Burgers',
+    url: 'bicky.png',
+  },
+  {
+    name: 'Bicky Cheeseburger',
+    price: 4.3,
+    category: 'Bicky Burgers',
+    url: 'bicky_cheese.png',
+  },
+  {
+    name: 'Bicky Royal Cheese',
+    price: 4.4,
+    category: 'Bicky Burgers',
+    url: 'royalcheese.png',
+  },
+  {
+    name: 'Bicky Ribburger',
+    price: 4.4,
+    category: 'Bicky Burgers',
+    url: 'mexicanoburger.png',
+  },
+  {
+    name: 'Chicken Fingers',
+    price: 4.1,
+    category: 'Chicken/Turkey Snacks',
+    url: 'chickenfingers.png',
+  },
+  {
+    name: 'Kippets',
+    price: 1,
+    category: 'Chicken/Turkey Snacks',
+    url: 'kippets.png',
+  },
+  {
+    name: 'Lucifer',
+    price: 4.2,
+    category: 'Chicken/Turkey Snacks',
+    url: 'lucifer.png',
+  },
+  {
+    name: 'Sweet Chick-in Chili',
+    price: 3.9,
+    category: 'Chicken/Turkey Snacks',
+    url: 'sweet_chicken.png',
+  },
+  {
+    name: 'Viandel',
+    price: 2.7,
+    category: 'Chicken/Turkey Snacks',
+    url: 'viandel_.png',
+  },
+];
+
+const mockCustomers = [
+  {
+    name: 'John',
+    email: 'john.doe@example.com',
+    password: 'password1',
+    passwordConfirm: 'password1',
+    role: 'USER',
+  },
+  {
+    name: 'Quinten',
+    email: 'Quinten@gmail.com',
+    password: 'StrongPassword1',
+    passwordConfirm: 'StrongPassword1',
+    role: 'ADMIN',
+  },
+  {
+    name: 'Maxim',
+    email: 'maxim@lison.be',
+    password: 'password7',
+    passwordConfirm: 'password7',
+    role: 'ADMIN',
+  },
+];
 
 async function main() {
   await prisma.orderItem.deleteMany({});
@@ -14,21 +301,7 @@ async function main() {
   await prisma.$queryRaw`alter table Category auto_increment = 1`;
   await prisma.$queryRaw`alter table Customer auto_increment = 1`;
 
-  const categorieNames = [
-    'Drinks',
-    'Fries',
-    'Dishes',
-    'Preperations',
-    'Cold Sauces',
-    'Hot Sauces',
-    'Snacks',
-    'Burgers',
-    'Bicky Burgers',
-    'Chicken/Turkey Snacks',
-    'Veggie/Fish Snacks',
-    'Cold Dishes',
-  ];
-
+  //seed all the categories
   const categories = [];
   for (const name of categorieNames) {
     const exists = await prisma.category.findFirst({
@@ -50,105 +323,7 @@ async function main() {
     }
   }
 
-  const productsData = [
-    {
-      name: 'Cola',
-      price: 2,
-      category: 'Drinks',
-      url: 'coke.png',
-    },
-    {
-      name: 'Fanta',
-      price: 2,
-      category: 'Drinks',
-      url: 'fanta.png',
-    },
-    {
-      name: 'Cola Zero',
-      price: 2,
-      category: 'Drinks',
-      url: 'coke_zero.png',
-    },
-    {
-      name: 'Sprite',
-      price: 2,
-      category: 'Drinks',
-      url: 'sprite.png',
-    },
-    {
-      name: 'Water',
-      price: 2,
-      category: 'Drinks',
-      url: 'water.png',
-    },
-    {
-      name: 'Water Bruis',
-      price: 2,
-      category: 'Drinks',
-      url: 'water_spark.png',
-    },
-    {
-      name: 'Ice Tea',
-      price: 2.2,
-      category: 'Drinks',
-      url: 'ice_tea.png',
-    },
-    {
-      name: 'Ice Tea Peach',
-      price: 2.2,
-      category: 'Drinks',
-      url: 'ice_tea_peach.png',
-    },
-    {
-      name: 'Ice Tea Zero',
-      price: 2.2,
-      category: 'Drinks',
-      url: 'ice_tea_zero.png',
-    },
-    {
-      name: 'Boulet',
-      price: 2.8,
-      category: 'Snacks',
-      url: 'boulet.png',
-    },
-    {
-      name: 'Bitterballen',
-      price: 2.8,
-      category: 'Snacks',
-      url: 'bitterbal.png',
-    },
-    {
-      name: 'Mini Friet',
-      price: 2.8,
-      category: 'Fries',
-      url: 'fries_mini.png',
-    },
-    {
-      name: 'Kleine Friet',
-      price: 3.3,
-      category: 'Fries',
-      url: 'fries_small.png',
-    },
-    {
-      name: 'Grote Friet',
-      price: 4,
-      category: 'Fries',
-      url: 'fries_big.png',
-    },
-    {
-      name: 'Super Friet',
-      price: 5.1,
-      category: 'Fries',
-      url: 'fries_super.png',
-    },
-    {
-      name: 'Curryworst',
-      price: 2.3,
-      category: 'Snacks',
-      url: 'frikandel.png',
-    },
-  ];
-
+  //seed all the products
   const products = [];
   for (const product of productsData) {
     const ProductExists = await prisma.product.findFirst({
@@ -180,78 +355,31 @@ async function main() {
     }
   }
 
-  const mockCustomers = [
-    {
-      name: 'John',
-      //firstName: 'John',
-      //lastName: 'Doe',
-      //birthDate: new Date('1990-01-15T00:00:00Z'),
-      email: 'john.doe@example.com',
-      //phone: '123-456-7890',
-      password: 'password1',
-    },
-    /* {
-      firstName: 'Jane',
-      lastName: 'Smith',
-      birthDate: new Date('1985-05-20T00:00:00Z'),
-      email: 'jane.smith@example.com',
-      phone: '987-654-3210',
-      password: 'password2',
-    },
-    {
-      firstName: 'Alice',
-      lastName: 'Johnson',
-      birthDate: new Date('1992-08-10T00:00:00Z'),
-      email: 'alice.j@example.com',
-      phone: '555-123-4567',
-      password: 'password3',
-    },
-    {
-      firstName: 'Bob',
-      lastName: 'Smith',
-      birthDate: new Date('1988-03-25T00:00:00Z'),
-      email: 'bob.smith@example.com',
-      phone: '555-987-6543',
-      password: 'password4',
-    },
-    {
-      firstName: 'Eve',
-      lastName: 'Adams',
-      birthDate: new Date('1995-12-03T00:00:00Z'),
-      email: 'eve.adams@example.com',
-      phone: '555-876-5432',
-      password: 'password5',
-    },
-    {
-      firstName: 'David',
-      lastName: 'Williams',
-      birthDate: new Date('1990-06-15T00:00:00Z'),
-      email: 'david.w@example.com',
-      phone: '555-234-5678',
-      password: 'password6',
-    }, */
-    {
-      name: 'Maxim',
-      //firstName: 'Maxim',
-      //lastName: 'Lison',
-      //birthDate: new Date('2002-05-07T12:30:30Z'),
-      email: 'maxim@lison.be',
-      //phone: '555-234-5678',
-      password: 'password7',
-    },
-  ];
-
+  //seed all the customers
   for (const customer of mockCustomers) {
-    await prisma.customer.create({
-      data: customer,
-    });
+    const authService = new AuthService(prismaService, jwtService);
+    await authService.register(customer);
   }
+
+  // Seed orders and order items
   const customers = await prisma.customer.findMany();
+
   const productsForOrders = await prisma.product.findMany();
 
   // Function to generate a random quantity between min and max
   function getRandomQuantity(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  // Function to generate a random date between 1 month ago and 1 month from now
+  function getRandomDate() {
+    const start = new Date();
+    start.setMonth(start.getMonth() - 1);
+    const end = new Date();
+    end.setMonth(end.getMonth());
+    return new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime()),
+    );
   }
 
   // Seed orders and order items
@@ -263,6 +391,7 @@ async function main() {
           customer: {
             connect: { id: customer.id },
           },
+          pickup: getRandomDate(),
         },
       });
 
